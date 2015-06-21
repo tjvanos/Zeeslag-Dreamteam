7563ab3c90957a13c947b349fa7a0cb5b0f794c1
tjvanos <tjos@student.avans.nl>
2015-06-21 14:59:51 +0200

    lijst

--- zeeslag/gameLijstVM.js
+++ zeeslag/gameLijstVM.js
@@ -1,12 +1,13 @@
-var xmlhttp= new XMLHttpRequest();
-xmlhttp.open("GET","https://zeeslagavans.herokuapp.com/users/me/games?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.InRqb3NAYXZhbnMubmwi.bYe_bj2RBgp4F71ZHxz4wWJ7R4DRmvPiYq8HdBGGzmg",false);
-xmlhttp.send();
-var test=JSON.parse(xmlhttp.responseText);
+var getGames= new XMLHttpRequest();
+getGames.open("GET","https://zeeslagavans.herokuapp.com/users/me/games?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.InRqb3NAYXZhbnMubmwi.bYe_bj2RBgp4F71ZHxz4wWJ7R4DRmvPiYq8HdBGGzmg",false);
+getGames.send();
+var games=JSON.parse(getGames.responseText);
+//https://zeeslagavans.herokuapp.com/games?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.InRqb3NAYXZhbnMubmwi.bYe_bj2RBgp4F71ZHxz4wWJ7R4DRmvPiYq8HdBGGzmg
 
-console.log(test);
-console.log(xmlhttp.responseText);
+console.log(games);
+console.log(getGames.responseText);
 
-for (var i = 0; i < test.length; i++) {
+for (var i = 0; i < games.length; i++) {
     var col = $('<div>');
     col.addClass("col-lg-3 col-sm-6 text-center");
     col.attr('id', "twee");
@@ -14,6 +15,16 @@ for (var i = 0; i < test.length; i++) {
         border: '1px solid black',
         margin: '5px'
     });
-    col.html("<h3>VS. Naam</h3><p>Test extra tekst</p>");
-    $('#een').append(col);
+    if(games[i].status!="que"){
+        col.html("<h3> VS. "+games[i].enemyName+"</h3>" +
+        "<p>Status: "+games[i].status+"</p>" +
+        "<a href='zeeslag.html'>Open spel ></a>");
+        $('#een').append(col);
+    }else{
+        col.html("<h3>Wachten op tegenstander... </h3>");
+        $('#een').append(col);
+    }
+
 }
+
+

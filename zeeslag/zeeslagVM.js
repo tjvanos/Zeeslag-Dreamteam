b55c4be587bccb167ccdcc6d6209136f80876708
Tijn Kanters <trkanter@avans.nl>
2015-06-22 15:34:58 +0200

    oke

--- zeeslag/zeeslagVM.js
+++ zeeslag/zeeslagVM.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
 
 var cordsx=["A","B","C","D","E","F","G","H","I","J"];
 
+var getShips= new XMLHttpRequest();
+getShips.open("GET","https://zeeslagavans.herokuapp.com/ships?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.InRqb3NAYXZhbnMubmwi.bYe_bj2RBgp4F71ZHxz4wWJ7R4DRmvPiYq8HdBGGzmg",false);
+getShips.send();
+var ships=JSON.parse(getShips.responseText);
+
 function coordinate(x, y, state) {
     this.x = x;
     this.y = y;
@@ -69,52 +74,15 @@ $('.cell').css({
 });
 
 var length=0;
-var isVertical=false;
+var isVertical = false;
 
 $(".cell").hover(
 
     function(){
 
         if(shipSelected != "none"){
-            if(shipSelected=="patrolboat"){
-                length=2;
-            }
-            if(shipSelected=="submarine"){
-                length=3;
-            }
-            if(shipSelected=="destroyer"){
-                length=3;
-            }
-            if(shipSelected=="battleship"){
-                length=4;
-            }
-            if(shipSelected=="carrier"){
-                length=5;
-            }
             var cell = $(this);
-            var coord = cell.data('cord');
-            var x=coord.x;
-            var y=coord.y;
-            var draw=true;
-            if(isVertical){
-
-            }else{
-                if((coord.x+(length-1))>9){
-                    draw=false;
-                }
-                var y=coord.y;
-                if(draw){
-
-                    for (var i = 0; i < length; i++)
-                    {
-                        var test= $('div[x='+x+'][y='+y+']');
-                        test.removeClass("off");
-                        test.addClass("boat");
-                        x++;
-                    }
-                }
-
-            }
+            drawShip(cell);
         }
         else{
             $(this).removeClass("off");
@@ -128,14 +96,19 @@ $(".cell").hover(
             var x=coord.x;
             var y=coord.y;
             if(isVertical){
-
+                for (var i = 0; i < length; i++) {
+                    var test = $('div[x=' + x + '][y=' + y + ']');
+                    test.removeClass("boat");
+                    test.addClass("off");
+                    y++;
+                }
             }else{
-                    for (var i = 0; i < length; i++) {
-                        var test = $('div[x=' + x + '][y=' + y + ']');
-                        test.removeClass("boat");
-                        test.addClass("off");
-                        x++;
-                    }
+                for (var i = 0; i < length; i++) {
+                    var test = $('div[x=' + x + '][y=' + y + ']');
+                    test.removeClass("boat");
+                    test.addClass("off");
+                    x++;
+                }
             }
         }
         else{
@@ -146,14 +119,10 @@ $(".cell").hover(
 });
 
 $(".board1").on('click', '.cell', function(event) {
+    if(shipSelected != "none"){
         var cell = $(this);
-        var className = cell.attr('class');
-        if (className.indexOf("checked") >= 0){
-            var coord = cell.data('cord');
-            alert("dit mag niet "+  cordsx[coord.x] + " , "+(1+coord.y));
-        }
-        cell.removeClass("off");
-        cell.addClass("checked");
+        drawShip(cell);
+    }
 });
 
 $(".board2").on('click', '.cell', function(event) {
@@ -179,4 +148,55 @@ function deselectShips(){
     $("#destroyer").css('font-weight', 'normal');
     $("#battleship").css('font-weight', 'normal');
     $("#carrier").css('font-weight', 'normal');
+}
+
+function rotateShip(){
+    if(isVertical == true){
+        isVertical = false;
+    }
+    else{
+        isVertical = true;
+    }
+}
+
+function drawShip(cell){
+    var ship = ships[shipSelected];
+    var length = ship.length;
+    var cell = cell;
+    var coord = cell.data('cord');
+    var x=coord.x;
+    var y=coord.y;
+    var draw=true;
+    if(isVertical){
+        if((coord.y+(length-1))>9){
+            draw=false;
+        }
+        var x=coord.x;
+        if(draw){
+
+            for (var i = 0; i < length; i++)
+            {
+                var test= $('div[x='+x+'][y='+y+']');
+                test.removeClass("off");
+                test.addClass("boat");
+                y++;
+            }
+        }
+    }
+    else{
+        if((coord.x+(length-1))>9){
+            draw=false;
+        }
+        var y=coord.y;
+        if(draw){
+
+            for (var i = 0; i < length; i++)
+            {
+                var test= $('div[x='+x+'][y='+y+']');
+                test.removeClass("off");
+                test.addClass("boat");
+                x++;
+            }
+        }
+    }
 }
